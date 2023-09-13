8d8c00cb96cbbe1a91b8012fa3ef40a350aa20d8
Victor Ajibade <adesojimav@gmail.com>
2023-09-13 13:49:42 +0100

    working on rider earning

--- ryder/src/App.js
+++ ryder/src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AppNavbar from "./components/landing_page/appNavbar";
-import LandingPage from "./components/landing_page/landingPage";
-import Footer from "./components/landing_page/footer";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import AppNavbar from "./components/landing_page/appNavbar";
+// import LandingPage from "./components/landing_page/landingPage";
+// import Footer from "./components/landing_page/footer";
 // Import require Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/styles/landingPage.css";
+import RyderEarnings from './components/ryderEarnings/earnings'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <AppNavbar />
-        <Routes>
-          <Route path="/" exact element={<LandingPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //     <AppNavbar />
+    //     <Routes>
+    //       <Route path="/" exact element={<LandingPage />} />
+    //     </Routes>
+    //     <Footer />
+    //   </div>
+    // </Router>
+
+    <RyderEarnings/>
+
   );
 }
 

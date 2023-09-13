2a410ab0ffa318339752dc1c8b7dda021f0a3325
mightysodiq <muritalasodiqbab1@gmail.com>
2023-09-13 15:54:47 +0100

    added password reset and passwordresetverification

--- ryder/src/App.js
+++ ryder/src/App.js
@@ -3,9 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppNavbar from "./components/landing_page/appNavbar";
 import LandingPage from "./components/landing_page/landingPage";
 import Footer from "./components/landing_page/footer";
+import Passwordreset from "./components/passwordresets/passwordreset";
 // Import require Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/styles/landingPage.css";
+import Passwordresetverification from "./components/passwordresets/Passwordresetverification";
+
 
 function App() {
   return (
@@ -14,10 +17,14 @@ function App() {
         <AppNavbar />
         <Routes>
           <Route path="/" exact element={<LandingPage />} />
+          <Route path="/passwordreset" exact element={<Passwordreset />} />
+          <Route path="/passwordresetverification" exact element={<Passwordresetverification/>} />
         </Routes>
         <Footer />
       </div>
     </Router>
+    
+    
   );
 }
 

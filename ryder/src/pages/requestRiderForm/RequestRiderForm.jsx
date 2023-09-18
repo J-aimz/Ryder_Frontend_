445e5fbed2d48431fc23368a1d4f4ac6e8e105b4
James Adah <adahjames11@gmail.com>
2023-09-18 08:15:40 +0100

    added auto suggestion

--- ryder/src/pages/requestRiderForm/RequestRiderForm.jsx
+++ ryder/src/pages/requestRiderForm/RequestRiderForm.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import styles from "./RequestRiderForm.module.css";
-import { backArrowIcon } from "../../assets";
+import React, { useState, useEffect } from "react";
+import styles from "./RequestRiderForm.module.css"
+import { backArrowIcon } from "../../assets"
 
-function RequestRiderForm() {
+const RequestRiderForm = () => {
   const [formData, setFormData] = useState({
     pickUpLocation: "",
     dropOffLocation: "",
@@ -10,34 +10,119 @@ function RequestRiderForm() {
     Offer: 0,
   });
 
-  function handleFormDataChange(e) {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  }
-  function handleFormsubmition() {
-    console.log(formData, "Form submited");
+  const [pickUpSuggestions, setPickUpSuggestions] = useState([]);
+  const [dropOffSuggestions, setDropOffSuggestions] = useState([]);
+  const [selectedPickUpLocation, setSelectedPickUpLocation] = useState("");
+  const [selectedDropOffLocation, setSelectedDropOffLocation] = useState("");
+  const [showPickUpSuggestions, setShowPickUpSuggestions] = useState(true)
+  const [showDropOffSuggestions, setShowDropOffSuggestions] = useState(true)
+
+  useEffect(() => {
+    if (formData.pickUpLocation) {
+      fetch(
+        `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${formData.pickUpLocation}&key=${process.env.REACT_APP_googleMapsKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.predictions) {
+            setPickUpSuggestions(data.predictions);
+          }
+        })
+        .catch((error) =>
+          console.error("Error fetching pickup location suggestions:", error)
+        );
+    } else {
+      setPickUpSuggestions([]);
+    }
+  }, [formData.pickUpLocation]);
+
+  useEffect(() => {
+    if (formData.dropOffLocation) {
+      fetch(
+        `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${formData.dropOffLocation}&key=${process.env.REACT_APP_googleMapsKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.predictions) {
+            setDropOffSuggestions(data.predictions);
+          }
+        })
+        .catch((error) =>
+          console.error("Error fetching drop-off location suggestions:", error)
+        );
+    } else {
+      setDropOffSuggestions([])
+    }
+  }, [formData.dropOffLocation])
+
+  const handlePickUpInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }))
+    setSelectedPickUpLocation(value)
+    setShowPickUpSuggestions(true)
+  };
+
+  const handleDropOffInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }))
+    setSelectedDropOffLocation(value)
+    setShowDropOffSuggestions(true)
+  };
+
+  const handleSelectPickUpLocation = (location) => {
+    setSelectedPickUpLocation(location.description);
+    setFormData((prev) => ({ ...prev, pickUpLocation: location.description }));
+    setShowPickUpSuggestions(false); 
   }
 
+  const handleSelectDropOffLocation = (location) => {
+    setSelectedDropOffLocation(location.description);
+    setFormData((prev) => ({ ...prev, dropOffLocation: location.description }));
+    setShowDropOffSuggestions(false)
+  }
+
+  const handlePickUpInputBlur = () => {
+    setShowPickUpSuggestions(false)
+  }
+
+  const handleDropOffInputBlur = () => {
+    setShowDropOffSuggestions(false)
+  }
+
+
   return (
-    <div className={styles.con}>
-          <div className={styles.body_container}>
-        <div className={styles.header}>
-          <div>
-            <img src={backArrowIcon} alt="" />
-            <span>Back</span>
-          </div>
-          <span>Request a Rider</span>
-        </div>
-        <div className={styles.form_con}>
+     <div className={styles.con}>
+        <div className={styles.body_container}>
+         <div className={styles.header}>
+           <div>
+             <img src={backArrowIcon} alt="" />
+             <span>Back</span>
+           </div>
+           <span>Request a Rider</span>
+         </div>
+          <div className={styles.form_con}>
           <label htmlFor="pickUpLocation">
             <span>Pick Up Location</span>
             <input
               type="text"
               id="pickUpLocation"
               name="pickUpLocation"
-              value={formData.pickUpLocation}
-              onChange={(e) => handleFormDataChange(e)}
+              value={selectedPickUpLocation}
+              onChange={handlePickUpInputChange}
+              onBlur={handlePickUpInputBlur}
             />
+            {showPickUpSuggestions && (
+              <ul className="suggestions">
+                {pickUpSuggestions.map((location) => (
+                  <li
+                    key={location.place_id}
+                    onClick={() => handleSelectPickUpLocation(location.description)}
+                  >
+                    {location.description}
+                  </li>
+                ))}
+              </ul>
+            )}
           </label>
 
           <label htmlFor="dropOffLocation">
@@ -46,9 +131,24 @@ function RequestRiderForm() {
               type="text"
               id="dropOffLocation"
               name="dropOffLocation"
-              value={formData.dropOffLocation}
-              onChange={(e) => handleFormDataChange(e)}
+              value={selectedDropOffLocation}
+              onChange={handleDropOffInputChange}
+              onBlur={handleDropOffInputBlur}
             />
+            {showDropOffSuggestions && (
+              <ul className="suggestions">
+                {dropOffSuggestions.map((location) => (
+                  <li
+                    key={location.place_id}
+                    onClick={() =>
+                      handleSelectDropOffLocation(location.description)
+                    }
+                  >
+                    {location.description}
+                  </li>
+                ))}
+              </ul>
+            )}
           </label>
 
           <label htmlFor="dropOffPhoneNumber">
@@ -58,7 +158,7 @@ function RequestRiderForm() {
               id="dropOffPhoneNumber"
               name="dropOffPhoneNumber"
               value={formData.dropOffPhoneNumber}
-              onChange={(e) => handleFormDataChange(e)}
+              onChange={handlePickUpInputChange}
             />
           </label>
 
@@ -69,17 +169,17 @@ function RequestRiderForm() {
               id="Offer"
               name="Offer"
               value={formData.Offer}
-              onChange={(e) => handleFormDataChange(e)}
+              onChange={handlePickUpInputChange}
             />
           </label>
 
-          <button type="button" onClick={handleFormsubmition}>
+          <button type="button" onClick={() => console.log(formData)}>
             Order Ride
           </button>
         </div>
       </div>
-    </div>
+      </div>
   );
-}
+};
 
 export default RequestRiderForm;

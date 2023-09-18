dc260e9f30d29f9d6084401fb85772f4e6158b9b
James Adah <adahjames11@gmail.com>
2023-09-18 17:11:08 +0100

    fixed error

--- ryder/src/pages/requestRiderForm/RequestRiderForm.jsx
+++ ryder/src/pages/requestRiderForm/RequestRiderForm.jsx
@@ -1,5 +1,5 @@
 import {useNavigate} from "react-router-dom"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./RequestRiderForm.module.css";
 import { backArrowIcon } from "../../assets";
 

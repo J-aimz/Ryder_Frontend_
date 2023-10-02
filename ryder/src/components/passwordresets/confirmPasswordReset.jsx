203019654317523e93dbcd71a9e4ab9a3e9cb928
CalebTek <olawale.odeyemi@decagon.dev>
2023-10-02 22:50:43 +0100

    Landing page bug fixed

--- ryder/src/components/passwordresets/confirmPasswordReset.jsx
+++ ryder/src/components/passwordresets/confirmPasswordReset.jsx
@@ -2,6 +2,18 @@
 import React from "react";
 import Email from "../../images/Email.svg";
 import "../../styles/confirmPasswordReset.css";
+import styled from "styled-components";
+
+// Define a styled component for the body
+const BodyContainer = styled.body`
+  font-family: "Manrope";
+  background-color: #f5f5f5;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 const ConfirmPasswordReset = () => {
   const handleResendEmail = () => {
@@ -10,40 +22,42 @@ const ConfirmPasswordReset = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="frame">
-          <img
-            className="img-fluid"
-            alt="Vector"
-            src={Email}
-            style={{ height: "72px", width: "90px", alignSelf: "center" }}
-          />
-          <h1 className="mt-3 text-center">Check your mail</h1>
-          <p className="text-center">
-            We sent a password reset link to your email. <br />
-            Please click the link to reset your password
-          </p>
-          <p className="text-center">
-            Didn't receive the email?{" "}
-            <a
-              href="#"
-              onClick={handleResendEmail}
-              style={{
-                textDecoration: "none",
-                color: "#FB8500"
-              }}
-            >
-              Click to Resend link
-            </a>
-          </p>
+    <BodyContainer>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="frame">
+            <img
+              className="img-fluid"
+              alt="Vector"
+              src={Email}
+              style={{ height: "72px", width: "90px", alignSelf: "center" }}
+            />
+            <h1 className="mt-3 text-center">Check your mail</h1>
+            <p className="text-center">
+              We sent a password reset link to your email. <br />
+              Please click the link to reset your password
+            </p>
+            <p className="text-center">
+              Didn't receive the email?{" "}
+              <a
+                href="#"
+                onClick={handleResendEmail}
+                style={{
+                  textDecoration: "none",
+                  color: "#FB8500"
+                }}
+              >
+                Click to Resend link
+              </a>
+            </p>
 
-          <div className="div-wrapper">
-            <button style={{ width: "204px" }}>Back to Login</button>
+            <div className="div-wrapper">
+              <button style={{ width: "204px" }}>Back to Login</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </BodyContainer>
   );
 };
 

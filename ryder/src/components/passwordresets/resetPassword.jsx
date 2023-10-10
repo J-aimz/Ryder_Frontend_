78aa4b297938c20e1efdc705d4e81ea5d2d2daea
mightysodiq <muritalasodiqbab1@gmail.com>
2023-10-10 16:46:25 +0100

    new commit

--- ryder/src/components/passwordresets/resetPassword.jsx
+++ ryder/src/components/passwordresets/resetPassword.jsx
@@ -1,171 +1,124 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import { useLocation } from "react-router-dom";
-
-import Bike from "../../images/Bike.svg";
+import styles from "../../styles/signUp.module.css";
+//import axios from "axios";
+import SignUpBanner from "../../images/RyderImg.svg";
 import RyderLogo from "../../images/Ryder-Logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-// Create a styled component for ResetPassword
-const ResetPasswordContainer = styled.div`
-  font-family: "Inter", sans-serif;
-`;
-
-// Create a styled component for input
-const StyledInput = styled.input`
-  width: 396px;
-  height: 48px;
-  padding: 9px 16px 8px 16px;
-  border-radius: 6px;
-  border: 1px solid;
-  gap: 16px;
-  opacity: 0.8;
-`;
-
-const ResetPassword = () => {
-  const secondDivStyle = {
-    width: "600px",
-    height: "456px",
-    top: "85px",
-    left: "894px",
-    gap: "80px"
-  };
-
-  const h1Style = {
-    width: "444px",
-    height: "234px",
-    color: "#FFFFFF",
-    fontSize: "32px",
-    fontWeight: "700",
-    lineHeight: "48px",
-    letterSpacing: "0em"
-  };
-
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-
-  const token = queryParams.get("token");
-  const email = queryParams.get("email");
-
-  //console.log("Token: ", token);
-  //console.log("Email: ", email);
-
-  const [newPassword, setNewPassword] = useState("");
+function ResetPassword() {
+  const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  const isPasswordValid = passwordRegex.test(password);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
 
-  const apiUrl = process.env.REACT_APP_API_URL_RESET_PASSWORD;
-  const requestData = {
-    newPassword: newPassword,
-    confirmPassword: confirmPassword,
-    resetToken: token,
-    email: email
-  };
+  function successMess(successMessage) {
+    toast.success(successMessage, {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  }
+  function errorMesage(error) {
+    toast.error(error, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  }
 
-  const handleResetPassword = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setError("");
-    setSuccessMessage("");
-
-    try {
-      const response = await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(requestData)
-      });
-
-      const data = await response.json();
-      //console.log("reset data: ", data);
-
-      if (!data.succeeded) {
-        setError(data.message);
-      } else {
-        setSuccessMessage("Password reset successfully");
-      }
-    } catch (error) {
-      console.error(error);
-      setError("An error occurred while resetting the password.");
-    } finally {
-      setLoading(false);
-    }
   };
-
   return (
-    <ResetPasswordContainer>
-      <div className="row">
-        <div
-          className="col-md-6"
-          style={{
-            backgroundImage: `url(${Bike})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            position: "relative"
-          }}
-        >
-          <div style={h1Style}>
-            Delivery service just got easier, elegant & superb with{" "}
-            <span style={{ color: "#FB8500" }}>Ryder</span>
+    <>
+      <div className={`${styles.wrapper} row`}>
+        <div className={`${styles.holder} col-md-12`}>
+          <div className={`${styles.left} col-md-7`}>
+            <img src={SignUpBanner} alt="" srcset="" height={700} />
           </div>
-        </div>
+          <div className={`${styles.right} col-md-5`}>
+            <div className={`${styles.content}`}>
+              <div className={`${styles.logoholder} mt-6`}>
+                {" "}
+                <img src={RyderLogo} alt="" srcset="" />
+              </div>
 
-        <div
-          className="col-md-6 d-flex align-items-center justify-content-center"
-          style={secondDivStyle}
-        >
-          <div>
-            <img
-              src={RyderLogo}
-              alt="ryder-img"
-              className="img-fluid mb-5 mt-5"
-            />
-            <p className="mb-4 fw-bold fs-5">Reset Password</p>
-            <div className="mb-3">
-              <label htmlFor="newPassword" className="form-label">
-                New Password
-              </label>
-              <StyledInput
-                type="password"
-                className="form-control"
-                id="newPassword"
-                placeholder="Enter your password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-              />
+              <form action="" method="post" className="elements">
+                <h3 className={`${styles.SignUp_H4} mt-4`}>Reset Password</h3>
+                <div className="form-holder col-md-7">
+                  <label className="mt-2">
+                    <b>Password</b>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    className="form-control mt-1"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="form-holder col-md-7">
+                  <label className="mt-2">
+                    <b>Confirm Password</b>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Confirm your password"
+                    className="form-control mt-1"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+
+                {/* Display success message */}
+                {successMessage && (
+                  <div className={`${styles.messages1}form-holder col-md-7`}>
+                    <small>
+                      <b>{successMessage}</b>
+                    </small>
+                  </div>
+                )}
+                {/* Display loading spinner */}
+                {loading && (
+                  <div className={`${styles.messages2}form-holder col-md-7`}>
+                    <small>Loading...</small>
+                  </div>
+                )}
+
+                <div className="form-holder col-md-7">
+                  <button
+                    className={`${styles.submitting}`}
+                    type="submit"
+                    onClick={handleRegister}
+                    //disabled={!isPasswordValid} // Disable button if password is not a match
+                  >
+                    {" "}
+                    Reset Password{" "}
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className="mb-4">
-              <label htmlFor="confirmPassword" className="form-label">
-                Confirm Password
-              </label>
-              <StyledInput
-                type="password"
-                className="form-control"
-                id="confirmPassword"
-                placeholder="Re-Enter your password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
-            <button onClick={handleResetPassword} disabled={loading}>
-              {loading ? "Resetting Password..." : "Reset Password"}
-            </button>
-            {error && <div style={{ color: "red" }}>{error}</div>}
-            {successMessage && (
-              <div style={{ color: "green" }}>{successMessage}</div>
-            )}
           </div>
         </div>
       </div>
-    </ResetPasswordContainer>
+      <ToastContainer />
+    </>
   );
-};
+}
 
 export default ResetPassword;

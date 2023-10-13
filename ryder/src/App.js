7859ed9f559cdbfa1f5e391b8904c3bef4d54c8f
Mistaweng <emmanuelgyang.1822@gmail.com>
2023-10-13 15:39:50 +0100

    Added routes and modified to customer-dashboard

--- ryder/src/App.js
+++ ryder/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./pages/landing_page/footer";
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
 import CustomerSignUp from "./components/sign_page/customerSignUp";
 import RiderSignUp from "./components/sign_page/riderSignUp";
 import Passwordreset from "./components/passwordresets/passwordreset";
@@ -10,6 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/styles/landingPage.css";
 import "../src/styles/dashboard.css";
 import { CustomerDashBoard, RequestRiderForm, LandingPage, RiderDashboard, RyderEarnings } from './pages';
+import ConfirmEmail from "./components/ConfirmEmail";
 
 function App() {
   return (
@@ -19,14 +19,15 @@ function App() {
         <Routes>
           <Route path="/" exact element={<LandingPage />} />
           <Route path="/login" element={<Login/>} />
-          <Route path="dashboard" element={<CustomerDashBoard />} />
-          <Route path="request-form" element={<RequestRiderForm />} />
-          <Route path="ryder-dasboard" element={<RiderDashboard />} />
+          <Route path="/customer-dashboard" element={<CustomerDashBoard />} />
+          <Route path="/request-form" element={<RequestRiderForm />} />
+          <Route path="/ryder-dasboard" element={<RiderDashboard />} />
           <Route path="/earnings" element={<RyderEarnings/>}/>
-          <Route path="/customerSignUp" exact element={<CustomerSignUp />} />
-          <Route path="/riderSignUp" exact element={<RiderSignUp />} />
-          <Route path="/passwordreset" exact element={<Passwordreset />} />
-          <Route path="/passwordresetverification" exact element={<Passwordresetverification/>} />
+          <Route path="/customerSignUp" element={<CustomerSignUp />} />
+          <Route path="/riderSignUp" element={<RiderSignUp />} />
+          <Route path="/passwordreset" element={<Passwordreset />} />
+          <Route path="/passwordresetverification" element={<Passwordresetverification/>} />
+          <Route path="/confirmEmail" element={<ConfirmEmail />} />
         </Routes>
 y
       </div>

6f415a6a271c730613cd68da072df02b3da821f1
Estarmani <zamaniesther@gmail.com>
2023-10-14 10:28:49 +0100

    integrated rider earnings

--- ryder/src/pages/ryderEarnings/RyderEarnings.jsx
+++ ryder/src/pages/ryderEarnings/RyderEarnings.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import '../../styles/ryderEarnings.css'
 import Vector from '../../images/Vector.svg';
 import Time from '../../images/Time.svg'
@@ -8,6 +9,32 @@ import RiderNavbar from '../../components/dashboard/riderNavbar'
 
 
 export default function RyderEarnings(){
+      const [earningsData, setEarningsData] = useState({
+        totalEarning: 0.00,
+        rides: 0,
+        totalDuration:'00:00:00',
+        list: []
+      });
+    
+      useEffect(() => {
+        // Make an HTTP GET request to your .NET API endpoint
+        const id = 'c1f7fdaf-752f-4cea-aa1a-5f039f7d4402'
+          axios.get(`https://localhost:7173/api/v1/Rider/Rider-Earnings/${id}`)
+            .then((res) => {setEarningsData({
+                totalEarning: res.data.data.totalEarning,
+                rides: res.data.data.totalRides,
+                totalDuration: res.data.data.totalRideDuration,
+                list: res.data.data.rides
+            })
+            .then((response) => response.json());
+        })
+            
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+          });
+      }, []);
+      console.log(earningsData.list);
+     
   return (
     <div>
      <RiderNavbar/>
@@ -16,23 +43,26 @@ export default function RyderEarnings(){
         <div className="card container">
             <div className='container show-earnings text-center py-3'>
                 <p className='myEarnings'>MY EARNINGS</p>
-                <h4 className='fs-3 fw-bold earning-amount'>N236,000</h4>
+                <h4 className='fs-3 fw-bold earning-amount'> N{earningsData.totalEarning.toFixed(2)}</h4>
                 <div className='container d-flex flex-row justify-content-evenly mt-4'>
                     <section>
                         <img src= {Vector} alt='car'/>
-                        <span className='ps-2'>112 Rides</span>
+                        <span className='ps-2'>{earningsData.rides} Rides</span>
                     </section>
 
                     <section>
                         <img src={Time} alt="time" />
-                        <span className='ps-2'>890hrs . 20mins</span>
+                        <span className='ps-2'>{earningsData.totalDuration}</span>
                     </section>
                 </div>
             </div>
             <div className="card-body">
-               <TodayEarnings/>
-               <TodayEarnings/>
-               <TodayEarnings/>
+                {earningsData.list.map((item) => (
+                <TodayEarnings 
+                date = {item.createdAt}
+                time = {item.createdAt}
+                amount = {item.amount.toFixed(2)}/>))}
+               
               
             </div>
         </div>
@@ -43,13 +73,14 @@ export default function RyderEarnings(){
 }
 
 
-function TodayEarnings(){
+function TodayEarnings({date, time, amount}){
     return (
         <div className='container child-container d-flex flex-row justify-content-between border shadow-sm mb-3'>
-            <span className='align-self-center today'><span className='fw-bold'>Today</span>, 10:15AM</span>
+            <span className='align-self-center today'><span className='fw-bold'>{new Date(date).toLocaleDateString()}</span>,
+            {new Date(time).toLocaleTimeString()} </span>
             <span className='lh-1 py-3'>
                 <p className='delivered border py-2 px-4'>Delivered</p>
-                <p className='ps-4'>N2,200.00</p>
+                <p className='ps-4'>N{amount}</p>
             </span>
         </div>
     )

015f8c6caf1402995d77dcd6345e5f1ab2c09f5d
David Ogwuche <133539148+codedvd@users.noreply.github.com>
2023-10-19 23:45:58 +0100

    Resolve Issues with Naming Conflicts

--- ryder/src/App.js
+++ ryder/src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login/Login";
 import CustomerSignUp from "./components/sign_page/customerSignUp";
 import RiderSignUp from "./components/sign_page/riderSignUp";
 import SignalRChat from "./components/SignalRChat";
+import ForgetPassword from "./components/passwordresets/forgetPassword";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/styles/landingPage.css";
 import "../src/styles/dashboard.css";
@@ -13,12 +14,9 @@ import {
   LandingPage,
   RiderDashboard,
 } from "./pages";
-import {
-  ForgetPassword,
-  ConfirmPasswordReset,
-  ResetPassword,
-} from "./components";
+import ConfirmPasswordReset from "./components/passwordresets/confirmPasswordReset"
 import VerifyEmail from "./components/verifyEmail";
+import ResetPassword from "./components/passwordresets/resetPassword";
 import RyderEarnings from "./pages/ryderEarnings/RyderEarnings";
 import VerificationConfirm from "./components/verificationConfirm";
 
@@ -28,11 +26,11 @@ function App() {
       <div>
         <Routes>
           <Route path="/" exact element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login/>} />
           <Route path="/customer-dashboard" element={<CustomerDashBoard />} />
           <Route path="/request-form" element={<RequestRiderForm />} />
-          <Route path="/ryder-dashboard" element={<RiderDashboard />} />
-          <Route path="/earnings" element={<RyderEarnings />} />
+          <Route path="/ryder-dasboard" element={<RiderDashboard />} />
+          <Route path="/earnings" element={<RyderEarnings/>}/>
           <Route path="/customer-signUp" exact element={<CustomerSignUp />} />
           <Route path="/rider-signup" exact element={<RiderSignUp />} />
           <Route path="/forget-password" exact element={<ForgetPassword />} />

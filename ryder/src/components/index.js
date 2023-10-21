22181679d3792d1c33fe35154b65e50e527ee08e
James Adah <adahjames11@gmail.com>
2023-10-21 15:25:50 +0100

    integration and validation

--- ryder/src/components/index.js
+++ ryder/src/components/index.js
@@ -5,3 +5,32 @@ export { default as AppNavbar } from "./AppNavbar"
 export { default as MapComponent } from "./map/MapComponent"
 export { default as RiderNavbar } from "./dashboard/RiderNavbar"
 export { default as UserNavbar } from "./dashboard/UserNavbar"
+
+
+
+
+
+
+
+// "pickUpLocation": {
+//     "city": "string",
+//         "state": "string",
+//             "postCode": "string",
+//                 "longitude": "string",
+//                     "latitude": "string",
+//                         "country": "string"
+// },
+// "dropOffLocation": {
+//     "city": "string",
+//         "state": "string",
+//             "postCode": "string",
+//                 "longitude": "string",
+//                     "latitude": "string",
+//                         "country": "string"
+// },
+// "pickUpPhoneNumber": "string",
+//     "packageDescription": "string",
+//         "referenceNumber": "string",
+//             "amount": 0,
+//                 "appUserId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+// }
--- ryder/src/pages/requestRiderForm/RequestRiderForm.jsx
+++ ryder/src/pages/requestRiderForm/RequestRiderForm.jsx
@@ -1,5 +1,6 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./RequestRiderForm.module.css";
 import { backArrowIcon } from "../../assets";
 
@@ -12,10 +13,15 @@ function RequestRiderForm() {
     pickUpLocation: "",
     dropOffLocation: "",
     dropOffPhoneNumber: "",
-    Offer: null,
-    pickupLatLog: {},
-    dropOffLatLog: {},
+    packageDescription: "",
+    Offer: "",
+    pickupLatLog: null,
+    dropOffLatLog: null,
   });
+
+  const [userId, setUserId] = useState(""); 
+  const [token, setToken] = useState(""); 
+
 
   const [pickUpSuggestions, setPickUpSuggestions] = useState([]);
   const [dropOffSuggestions, setDropOffSuggestions] = useState([]);
@@ -23,6 +29,22 @@ function RequestRiderForm() {
   const [selectedDropOffLocation, setSelectedDropOffLocation] = useState("");
   const [showPickUpSuggestions, setShowPickUpSuggestions] = useState(false);
   const [showDropOffSuggestions, setShowDropOffSuggestions] = useState(false);
+
+  useEffect(() => { 
+    setUserId(localStorage.getItem("userId"));
+    setToken(localStorage.getItem("token"));
+  }, []);
+
+  useEffect(() => {
+    if (formData.pickupLatLog !== null && formData.dropOffLatLog !== null) {
+      postRequest(setData(formData))
+    }
+    
+  }, [formData.pickupLatLog, formData.dropOffLatLog]);
+
+  // const [enableBtn, setEnableBtn] = useState(false);
+
+
 
   useEffect(() => {
     if (formData.pickUpLocation) {
@@ -90,32 +112,235 @@ function RequestRiderForm() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
+    const validatedValue = formValidationOnChange(name, value);
+    setFormData((prev) => ({ ...prev, [name]: validatedValue }));
   };
+
+
+
+
+  function formValidationOnChange(name, value) {
+    if (name === "dropOffPhoneNumber") {
+      let validated = value.replace(/[^\d]/g, "");
+      let formatted = "";
+
+      if (validated.startsWith("234")) {
+        if (validated.length > 13) {
+          validated = validated.slice(0, 13);
+        }
+        formatted = `+${validated.slice(0, 3)}`;
+        if (validated.length > 3) {
+          formatted += ` ${validated.slice(3, 6)}`;
+        }
+        if (validated.length > 6) {
+          formatted += ` ${validated.slice(6, 9)}`;
+        }
+        if (validated.length > 9) {
+          formatted += ` ${validated.slice(9)}`;
+        }
+      } else if (validated.startsWith("0")) {
+        if (validated.length > 11) {
+          validated = validated.slice(0, 11);
+        }
+        formatted = `${validated.slice(0, 4)}`;
+        if (validated.length > 4) {
+          formatted += ` ${validated.slice(4, 7)}`;
+        }
+        if (validated.length > 7) {
+          formatted += ` ${validated.slice(7)}`;
+        }
+      } else {
+        return value;
+      }
+
+      return formatted;
+    } else if (name === "Offer") {
+      value = value.replace(/[^\d]/g, "");
+      value = value.replace(/,/g, "");
+
+      if (value.length > 3) {
+        let formatted = "";
+        let count = 0;
+
+        for (let i = value.length - 1; i >= 0; i--) {
+          if (count > 0 && count % 3 === 0) {
+            formatted = "," + formatted;
+            count = 0;
+          }
+          formatted = value[i] + formatted;
+          count++;
+        }
+        return formatted;
+      }
+
+      return value;
+    } else {
+      return value;
+    }
+  }
+
+
 
   const getLocationDetails = async (placeId) => {
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${process.env.REACT_APP_googleMapsKey}`
-    );
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${process.env.REACT_APP_googleMapsKey}`,
+        {
+          method: "GET",
+          mode: "cors",
+        }
+      );
+      const data = await response.json();
 
-    return data.result;
+      return data.result;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const handleFormSubmition = async () => {
-    const pickupLatLog = await getLocationDetails(
-      formData.pickUpLocation?.place_id
-    );
+  const handlePickUpLocationBlur = () => {
+    setTimeout(() => {
+      setShowPickUpSuggestions(false);
+    }, 500);
 
-    const dropOffLatLog = await getLocationDetails(
-      formData.dropOffLocation?.place_id
-    );
+    return clearTimeout();
+  };
 
-    setFormData((prev) => ({
-      ...prev,
-      pickupLatLog: pickupLatLog.geometry.location,
-      dropOffLatLog: dropOffLatLog.geometry.location,
-    }));
+  const handleDropOffLocationBlur = () => {
+    setTimeout(() => {
+      setShowDropOffSuggestions(false);
+    }, 500);
+
+    return clearTimeout();
+  };
+
+ 
+
+  async function postRequest(requestBody) {
+    try {
+     
+      const response = await fetch(
+        "https://ryder-test.onrender.com/api/v1/Order/placeOrder",
+        {
+          method: "POST",
+          mode: "cors",
+          cache: "no-cache",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(requestBody),
+        }
+      );
+
+      if (response.ok && response.status === 200) {
+        alert("Order Placed successfully")
+        navigate("/dashboard")
+      } else {
+        alert("Oops Somthing Went Wrong Refreash the Page and Try Again")
+      }
+    } catch (error) {
+      console.error("An error occurred while submitting the order:", error);
+    }
+  }
+
+  function setData(formData) {
+    const data = {
+      pickUpLocation: {
+        city:
+          formData?.pickUpLocation.terms[
+            formData.pickUpLocation?.terms?.length - 3
+          ]?.value || "",
+        state:
+          formData.pickUpLocation?.terms[
+            formData.pickUpLocation?.terms?.length - 2
+          ]?.value || "",
+        postCode: "string", 
+        longitude: `${formData.pickupLatLog?.lng}`,
+        latitude: `${formData?.pickupLatLog?.lat}`,
+        country:
+          formData.pickUpLocation?.terms[
+            formData.pickUpLocation.terms?.length - 1
+          ]?.value,
+        AddressDescription:
+          formData.pickUpLocation?.description || formData.pickUpLocation,
+      },
+      dropOffLocation: {
+        city: formData?.dropOffLocation?.terms[
+          formData.pickUpLocation.terms?.length - 3
+        ]?.value,
+        state:
+          formData.dropOffLocation?.terms[
+            formData.pickUpLocation.terms?.length - 2
+          ]?.value,
+        postCode: "string", 
+        longitude: `${formData?.dropOffLatLog?.lng}`,
+        latitude: `${formData?.dropOffLatLog?.lat}`,
+        country:
+          formData.dropOffLocation?.terms[
+            formData.pickUpLocation.terms?.length - 1
+          ]?.value,
+        AddressDescription:
+          formData.pickUpLocation.description || formData.pickUpLocation,
+      },
+      pickUpPhoneNumber: formData.dropOffPhoneNumber.replace(/\s/g, ""),
+      packageDescription: formData.packageDescription,
+      referenceNumber: "string",
+      amount: formData.Offer.replace(/,/g, ""), 
+      appUserId: userId,
+    };
+
+    return data;
+  }
+
+  
+  const entireFormValidation = () => {
+    let phone = formData.dropOffPhoneNumber.trim().replace(/[^\d]/g, "");
+      
+    const isPhoneValid =
+      ((phone.startsWith("0") && phone.length <= 11) ||
+      (phone.startsWith("234") && phone.length <= 13) ||
+      (phone.startsWith("+234") && phone.length <= 14))
+
+    if (
+      formData.pickUpLocation &&
+      formData.dropOffLocation &&
+      isPhoneValid &&
+      formData.packageDescription &&
+      formData.Offer
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+
+  const handleFormSubmission = async () => {
+    const dataIsValid = entireFormValidation()
+
+    console.log(dataIsValid, "data validity")
+    if (dataIsValid) {
+      
+      const pickupLatLog = await getLocationDetails(
+        formData.pickUpLocation?.place_id
+      );
+  
+      const dropOffLatLog = await getLocationDetails(
+        formData.dropOffLocation?.place_id
+      );
+  
+      setFormData((prev) => {
+        return {
+          ...prev,
+          pickupLatLog: pickupLatLog.geometry.location,
+          dropOffLatLog: dropOffLatLog.geometry.location,
+        };
+      });
+    } else {
+      console.log("Oops")
+    }
   };
 
   return (
@@ -134,11 +359,13 @@ function RequestRiderForm() {
             <label htmlFor="pickUpLocation">
               <span>Pick Up Location</span>
               <input
+                required
                 type="text"
                 id="pickUpLocation"
                 name="pickUpLocation"
                 value={selectedPickUpLocation?.description}
                 onChange={handlePickUpInputChange}
+                onBlur={handlePickUpLocationBlur}
               />
               {showPickUpSuggestions && (
                 <ul
@@ -159,11 +386,13 @@ function RequestRiderForm() {
             <label htmlFor="dropOffLocation">
               <span>Drop off Location</span>
               <input
+                required
                 type="text"
                 id="dropOffLocation"
                 name="dropOffLocation"
                 value={selectedDropOffLocation?.description}
                 onChange={handleDropOffInputChange}
+                onBlur={handleDropOffLocationBlur}
               />
               {showDropOffSuggestions && (
                 <ul
@@ -184,6 +413,7 @@ function RequestRiderForm() {
             <label htmlFor="dropOffPhoneNumber">
               <span>Drop off Phone Number</span>
               <input
+                required
                 type="tel"
                 id="dropOffPhoneNumber"
                 name="dropOffPhoneNumber"
@@ -191,17 +421,32 @@ function RequestRiderForm() {
                 onChange={(e) => handleChange(e)}
               />
             </label>
+            <label htmlFor="packageDescription">
+              <span>Package Description</span>
+              <input
+                required
+                type="text"
+                id="packageDescription"
+                name="packageDescription"
+                value={formData.packageDescription}
+                onChange={(e) => handleChange(e)}
+              />
+            </label>
             <label htmlFor="Offer">
               <span>Offer (NGN)</span>
               <input
-                type="number"
+                required
+                type="text"
                 id="Offer"
                 name="Offer"
                 value={formData.Offer}
                 onChange={(e) => handleChange(e)}
               />
             </label>
-            <button type="button" onClick={handleFormSubmition}>
+            <div>
+              <small></small>
+            </div>
+            <button type="button" onClick={handleFormSubmission}>
               Order Ride
             </button>
           </div>

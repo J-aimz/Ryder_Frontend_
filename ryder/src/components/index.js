d554d35062101f863c1aded6d5650ec18f45e14f
James Adah <adahjames11@gmail.com>
2023-10-21 16:04:20 +0100

    a

--- ryder/src/components/index.js
+++ ryder/src/components/index.js
@@ -4,4 +4,4 @@ export { default as Messages } from "./messages/Messages";
 export { default as AppNavbar } from "./AppNavbar";
 export { default as MapComponent } from "./map/MapComponent";
 export { default as RiderNavbar } from "./dashboard/riderNavbar";
-export { default as UserNavbar } from "./dashboard/UserNavbar";
+export { default as UserNavbar } from "./dashboard/userNavbar";
